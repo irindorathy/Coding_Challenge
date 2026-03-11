c6ff340b7410161928e9cbc1587042a6d289c3d0
thulasimrts-hub <thulasimrts@gmail.com>
2026-03-11 12:36:54 +0530

    thulasi.c

---
+++ thulasi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid;  // found
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;  // search right half
+        }
+        else {
+            high = mid - 1; // search left half
+        }
+    }
+    return -1; // not found
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key;
+
+    printf("Enter number to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
