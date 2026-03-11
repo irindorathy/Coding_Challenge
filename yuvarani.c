77b66470a0ab7e1308b85d492fed3ac1a1c010cf
Yuvarani2007 <yuvaranig721@gmail.com>
2026-03-11 12:33:49 +0530

    Create yuvarani.c

---
+++ yuvarani.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], i, j, temp;
+
+    printf("Enter 10 numbers:\n");
+
+    // Input numbers
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort
+    for(i = 0; i < 9; i++) {
+        for(j = i + 1; j < 10; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Display sorted numbers
+    printf("Sorted numbers:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
