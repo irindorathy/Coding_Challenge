14487ae543f3fe63eb2e4d1b3f6c60fce6802078
thareneCodes <thareneshree@gmail.com>
2026-03-11 12:56:06 +0530

    Create tharene.c

---
+++ tharene.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], i, j, temp;
+
+    printf("Enter 10 numbers:\n");
+    for(i = 0; i < 10; i++) {    // ✅ fixed
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble sort
+    for(i = 0; i < 10; i++) {
+        for(j = i + 1; j < 10; j++) {   // ✅ fixed
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted numbers:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+gcc sorting_error_irin.c -o sorting_irin
