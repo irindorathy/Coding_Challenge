6eeb29d7c65f7001178142730aa26ca6d55471d4
sankarmariappan83-hue <sankarmariappan83@gmail.com>
2026-03-11 12:39:51 +0530

    Update sankar18.c

--- sankar18.c
+++ sankar18.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int arr[10], i, j, temp;
+
+    printf("Enter 10 numbers:\n");
+
+    // Correct input loop
+    for(i = 0; i < 10; i++) {     // ✔ fixed condition
+        scanf("%d", &arr[i]);
+    }
+
+    // Correct Bubble Sort
+    for(i = 0; i < 9; i++) {
+        for(j = 0; j < 9 - i; j++) {   // ✔ correct bubble sort logic
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted numbers:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
