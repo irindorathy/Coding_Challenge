2b109042720a70ae50b24c748c9cb370b318bb23
palanipavithra750-droid <palanipavithra750@gmail.com>
2026-03-11 12:46:20 +0530

    Implement bubble sort for an array of numbers

--- palanipavithra.c
+++ palanipavithra.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], i, j, temp;
+
+    printf("Enter 10 numbers:\n");
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort (adjacent comparisons)
+    for(i = 0; i < 9; i++) {
+        for(j = 0; j < 9 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted numbers:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
 
